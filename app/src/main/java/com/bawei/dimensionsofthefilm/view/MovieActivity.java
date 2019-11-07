ca4339b7847567e7b96d03cb28e85a4fa0353982
yangjingyuy <54694493+yangjingyuy@users.noreply.github.com>
2019-11-07 10:03:11 +0800

    更改

--- app/src/main/java/com/bawei/dimensionsofthefilm/view/MovieActivity.java
+++ app/src/main/java/com/bawei/dimensionsofthefilm/view/MovieActivity.java
@@ -1,13 +1,27 @@
 package com.bawei.dimensionsofthefilm.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.widget.RadioGroup;
 
 import com.bawei.dimensionsofthefilm.R;
 import com.bawei.dimensionsofthefilm.base.BaseActivity;
+import com.bawei.dimensionsofthefilm.fragment.CinemaFragment;
+import com.bawei.dimensionsofthefilm.fragment.MovieFragment;
+import com.bawei.dimensionsofthefilm.fragment.MyFragment;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieActivity extends BaseActivity {
 
+    @BindView(R.id.movie_vp)
+    ViewPager movieVp;
+    private ArrayList<Fragment> list = new ArrayList<>();
 
     @Override
     protected int getLayoutId() {
@@ -17,8 +31,24 @@ public class MovieActivity extends BaseActivity {
     @Override
     protected void initView() {
 
-        //刚写得
+        //添加fragemnt
+        list.add(new MovieFragment());
+        list.add(new CinemaFragment());
+        list.add(new MyFragment());
+        movieVp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return list.get(i);
+            }
 
-
+            @Override
+            public int getCount() {
+                return list.size();
+            }
+        });
     }
+
+
+
+
 }
