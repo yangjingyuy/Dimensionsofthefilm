1e38361bdeea802a6fe581a59e2f7174dcf5b3e6
yangjingyuy <54694493+yangjingyuy@users.noreply.github.com>
2019-11-08 13:30:48 +0800

    2019.11.8,13.30

--- app/src/main/java/com/bawei/dimensionsofthefilm/fragment/CinemaFragment.java
+++ app/src/main/java/com/bawei/dimensionsofthefilm/fragment/CinemaFragment.java
@@ -1,5 +1,11 @@
 package com.bawei.dimensionsofthefilm.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,12 +13,25 @@ import android.view.ViewGroup;
 import com.bawei.dimensionsofthefilm.R;
 import com.bawei.dimensionsofthefilm.base.BaseFragmente;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /*时间:2019/11/6
 创建人:yang 
 创建人:杨靖宇*/
 public class CinemaFragment extends BaseFragmente {
 
+    @BindView(R.id.cinema_tab)
+    TabLayout cinemaTab;
+    @BindView(R.id.cinema_vp)
+    ViewPager cinemaVp;
+    Unbinder unbinder;
     private View view;
+    private ArrayList<Fragment> list = new ArrayList<>();
+    private ArrayList<String> listtab = new ArrayList<>();
 
     @Override
     public View getLayoutID(LayoutInflater inflater, ViewGroup container) {
@@ -22,6 +41,45 @@ public class CinemaFragment extends BaseFragmente {
 
     @Override
     public void initView() {
+        list.add(new RecommendFragment());
+        list.add(new NearbyFragment());
+        list.add(new LocationFragment());
+        listtab.add("推荐影院");
+        listtab.add("附近影院");
+        listtab.add("海淀区");
+        cinemaVp.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return list.get(i);
+            }
 
+            @Override
+            public int getCount() {
+                return list.size();
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return listtab.get(position);
+            }
+        });
+        cinemaTab.setupWithViewPager(cinemaVp);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
