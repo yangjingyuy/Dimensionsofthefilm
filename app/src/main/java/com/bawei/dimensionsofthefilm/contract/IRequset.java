b6f9014cd29b8e32ee88c4b53f653d6ea45c4768
yangjingyuy <54694493+yangjingyuy@users.noreply.github.com>
2019-11-08 10:11:43 +0800

    2019.11.8,10.11

--- app/src/main/java/com/bawei/dimensionsofthefilm/contract/IRequset.java
+++ app/src/main/java/com/bawei/dimensionsofthefilm/contract/IRequset.java
@@ -2,6 +2,7 @@ package com.bawei.dimensionsofthefilm.contract;
 
 import com.bawei.dimensionsofthefilm.model.Banner;
 import com.bawei.dimensionsofthefilm.model.Data;
+import com.bawei.dimensionsofthefilm.model.Hot;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /*时间:2019/11/6
 创建人:yang 
@@ -25,4 +27,7 @@ public interface IRequset {
     @GET("tool/v2/banner")
     Observable<Data<List<Banner>>> banner();
 
+    //热门电影列表
+    @GET("movie/v2/findHotMovieList")
+    Observable<Data<List<Hot>>> findHotMovieList(@Query("page")int page,@Query("count")int count);
 }
--- app/src/main/java/com/bawei/dimensionsofthefilm/fragment/MovieFragment.java
+++ app/src/main/java/com/bawei/dimensionsofthefilm/fragment/MovieFragment.java
@@ -1,24 +1,43 @@
 package com.bawei.dimensionsofthefilm.fragment;
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bawei.dimensionsofthefilm.R;
+import com.bawei.dimensionsofthefilm.adapter.HotAdapter;
 import com.bawei.dimensionsofthefilm.base.BaseFragmente;
 import com.bawei.dimensionsofthefilm.contract.DataColl;
 import com.bawei.dimensionsofthefilm.model.Banner;
 import com.bawei.dimensionsofthefilm.model.Data;
+import com.bawei.dimensionsofthefilm.model.Hot;
 import com.bawei.dimensionsofthefilm.presenter.BannerPresenter;
+import com.bawei.dimensionsofthefilm.presenter.HotPresenter;
+import com.bumptech.glide.Glide;
+import com.stx.xhb.xbanner.XBanner;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /*时间:2019/11/6
 创建人:yang 
 创建人:杨靖宇*/
 public class MovieFragment extends BaseFragmente {
 
+    @BindView(R.id.xbander)
+    XBanner xbander;
+    Unbinder unbinder;
+    @BindView(R.id.recy_hot)
+    RecyclerView recyHot;
     private View view;
+    private HotAdapter hotAdapter;
 
     @Override
     public View getLayoutID(LayoutInflater inflater, ViewGroup container) {
@@ -28,15 +47,62 @@ public class MovieFragment extends BaseFragmente {
 
     @Override
     public void initView() {
-        BannerPresenter bannerPresenter=new BannerPresenter(new bannerColl());
+        //轮播图
+        BannerPresenter bannerPresenter = new BannerPresenter(new bannerColl());
         bannerPresenter.reqsuetData();
+        //热门电影
+        HotPresenter hotPresenter = new HotPresenter(new hotColl());
+        hotPresenter.reqsuetData(1, 5);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        hotAdapter = new HotAdapter(getContext());
+        recyHot.setAdapter(hotAdapter);
+        recyHot.setLayoutManager(linearLayoutManager);
+
+
 
 
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
     //轮播图
-    class bannerColl implements DataColl<List<Banner>>{
+    class bannerColl implements DataColl<List<Banner>> {
         @Override
-        public void suuess(List<Banner> rese) {
+        public void suuess(final List<Banner> rese) {
+
+            xbander.setData(rese, null);
+            xbander.setmAdapter(new XBanner.XBannerAdapter() {
+                @Override
+                public void loadBanner(XBanner banner, View view, int position) {
+                    Glide.with(getContext()).load(rese.get(position).imageUrl).into((ImageView) view);
+                }
+            });
+
+        }
+
+        @Override
+        public void failure(Data data) {
+
+        }
+    }
+    //热门电影
+    class hotColl implements DataColl<List<Hot>> {
+        @Override
+        public void suuess(List<Hot> rese) {
+            hotAdapter.addAll(rese);
+            hotAdapter.notifyDataSetChanged();
 
         }
 
