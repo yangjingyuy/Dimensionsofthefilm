1e8ce13690c3b9b929056a46eece17bf0adc766e
yangjingyuy <54694493+yangjingyuy@users.noreply.github.com>
2019-11-19 20:12:47 +0800

    11,19更改

---
+++ app/src/main/java/com/bawei/dimensionsofthefilm/model/Inquiree.java
@@ -0,0 +1,14 @@
+package com.bawei.dimensionsofthefilm.model;
+
+/*时间:2019/11/15
+创建人:yang 
+创建人:杨靖宇*/
+public class Inquiree {
+    public String  director;
+    public String  imageUrl;
+    public int  movieId;
+    public String  name;
+    public String  starring;
+    public double score;
+
+}
